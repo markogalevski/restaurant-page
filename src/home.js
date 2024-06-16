4fa726aa6cf03adefa2f5afd8b9e08214a01456c
Marko Galevski <galevski.marko+github@gmail.com>
2024-06-16 13:37:52 +0300

    added medium image

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
-import img from '../src/splash.jpg';
+import imgMedium from '../src/splashMedium.jpeg';
 export function homeContents(content) {
 	content.innerHTML = `
       <h1>Čiurlionio 72</h1>
-      <img src=${img} alt="someart" />
+      <img src=${imgMedium} alt="someart" />
       <div class="info">
         <div class="booking">booking@ciurliono72.com</div>
         <div class="address">Čiurlionio Gatvė 72, Vilnius</div>
