3fb5254629c8bdbd077140158db9745b8933c66d
Marko Galevski <galevski.marko+github@gmail.com>
2024-06-16 13:08:17 +0300

    updated home.js s naming of image

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
-import Splash from '../src/splash.jpg';
+import img from '../src/splash.jpg';
 export function homeContents(content) {
 	content.innerHTML = `
       <h1>Čiurlionio 72</h1>
-      <img src=${Splash} alt="someart" />
+      <img src=${img} alt="someart" />
       <div class="info">
         <div class="booking">booking@ciurliono72.com</div>
         <div class="address">Čiurlionio Gatvė 72, Vilnius</div>
