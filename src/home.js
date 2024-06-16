d72be73d8f7e47ef64094c0af85fb627dbb2763a
Marko Galevski <galevski.marko+github@gmail.com>
2024-06-16 13:03:57 +0300

    updated image loading to be webpack'd

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
+import Splash from '../src/splash.jpg';
 export function homeContents(content) {
 	content.innerHTML = `
       <h1>Čiurlionio 72</h1>
-      <img src="../src/splash.jpg" alt="someart" />
+      <img src=${Splash} alt="someart" />
       <div class="info">
         <div class="booking">booking@ciurliono72.com</div>
         <div class="address">Čiurlionio Gatvė 72, Vilnius</div>
